f83fbef1feab30df496cb9a23f06dc2e57f4909a
Olya <belyaevaolga881@gmail.com>
2020-10-04 22:36:06 +0300

    init

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        struct DataItem
+        {
+            public int x;
+            public int y;
+        }
+
+        struct Grid1d { }
+
+        class V2Data { }
+
+        class V2DataOnGrid : 
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
